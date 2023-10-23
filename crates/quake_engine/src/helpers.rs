f756a2b212063c7805d37cbadb220908b03cb62e
cassaundra <cass@cassaundra.org>
2023-10-23 10:36:55 -0700

    Move source evaluation to custom engine internals

--- crates/quake_engine/src/helpers.rs
+++ crates/quake_engine/src/helpers.rs
@@ -5,10 +5,9 @@ use std::path::{Path, PathBuf};
 
 use nu_cmd_lang::create_default_context;
 use nu_command::add_shell_command_context;
-use nu_engine::{eval_block, eval_block_with_early_return};
-use nu_parser::parse;
-use nu_protocol::engine::{EngineState, Stack, StateWorkingSet};
-use nu_protocol::{print_if_stream, report_error, report_error_new, PipelineData, Span, Value};
+
+use nu_protocol::engine::{EngineState, Stack};
+use nu_protocol::{Span, Value};
 
 pub fn set_last_exit_code(stack: &mut Stack, exit_code: i64) {
     stack.add_env_var(
@@ -21,83 +20,6 @@ pub fn get_init_cwd() -> Option<PathBuf> {
     std::env::current_dir()
         .ok()
         .or_else(|| std::env::var("PWD").ok().map(Into::into))
-}
-
-pub fn eval_source(
-    engine_state: &mut EngineState,
-    stack: &mut Stack,
-    source: &[u8],
-    fname: &str,
-    input: PipelineData,
-    allow_return: bool,
-) -> bool {
-    let (block, delta) = {
-        let mut working_set = StateWorkingSet::new(engine_state);
-        let output = parse(
-            &mut working_set,
-            Some(fname), // format!("entry #{}", entry_num)
-            source,
-            false,
-        );
-        if let Some(err) = working_set.parse_errors.first() {
-            set_last_exit_code(stack, 1);
-            report_error(&working_set, err);
-            return false;
-        }
-
-        (output, working_set.render())
-    };
-
-    if let Err(err) = engine_state.merge_delta(delta) {
-        set_last_exit_code(stack, 1);
-        report_error_new(engine_state, &err);
-        return false;
-    }
-
-    let b = if allow_return {
-        eval_block_with_early_return(engine_state, stack, &block, input, false, false)
-    } else {
-        eval_block(engine_state, stack, &block, input, false, false)
-    };
-
-    match b {
-        Ok(pipeline_data) => {
-            let result;
-            if let PipelineData::ExternalStream {
-                stdout: stream,
-                stderr: stderr_stream,
-                exit_code,
-                ..
-            } = pipeline_data
-            {
-                result = print_if_stream(stream, stderr_stream, false, exit_code);
-            } else {
-                result = pipeline_data.print(engine_state, stack, true, false);
-            }
-
-            match result {
-                Err(err) => {
-                    report_error_new(engine_state, &err);
-                    return false;
-                }
-                Ok(exit_code) => {
-                    set_last_exit_code(stack, exit_code);
-                }
-            }
-
-            // reset vt processing, aka ansi because illbehaved externals can break it
-            #[cfg(windows)]
-            {
-                let _ = enable_vt_processing();
-            }
-        }
-        Err(err) => {
-            set_last_exit_code(stack, 1);
-            report_error_new(engine_state, &err);
-            return false;
-        }
-    }
-    true
 }
 
 pub fn create_engine_state() -> EngineState {
--- crates/quake_engine/src/lib.rs
+++ crates/quake_engine/src/lib.rs
@@ -1,7 +1,9 @@
 use std::fs;
 
-use nu_protocol::engine::{EngineState, Stack};
-use nu_protocol::PipelineData;
+use nu_engine::eval_block;
+use nu_parser::parse;
+use nu_protocol::engine::{EngineState, Stack, StateWorkingSet};
+use nu_protocol::{print_if_stream, report_error, report_error_new, PipelineData};
 
 use quake_core::prelude::*;
 
@@ -10,9 +12,9 @@ pub use helpers::*;
 
 #[derive(Clone)]
 pub struct Engine {
-    pub project: Project,
-    pub engine_state: EngineState,
-    pub stack: Stack,
+    project: Project,
+    engine_state: EngineState,
+    stack: Stack,
 }
 
 impl Engine {
@@ -27,18 +29,82 @@ impl Engine {
         })
     }
 
-    pub fn run(&mut self) -> Result<()> {
+    pub fn project(&self) -> &Project {
+        &self.project
+    }
+
+    pub fn run(&mut self) -> Result<bool> {
         let source = fs::read_to_string(self.project.build_script())
             .into_diagnostic()
             .wrap_err("Failed to read build script")?;
-        eval_source(
-            &mut self.engine_state,
+        Ok(self.eval_source(source.as_bytes(), "build.quake"))
+    }
+
+    fn eval_source(&mut self, source: &[u8], filename: &str) -> bool {
+        let (block, delta) = {
+            let mut working_set = StateWorkingSet::new(&self.engine_state);
+            let output = parse(&mut working_set, Some(filename), source, false);
+            if let Some(err) = working_set.parse_errors.first() {
+                set_last_exit_code(&mut self.stack, 1);
+                report_error(&working_set, err);
+                return false;
+            }
+
+            (output, working_set.render())
+        };
+
+        if let Err(err) = self.engine_state.merge_delta(delta) {
+            set_last_exit_code(&mut self.stack, 1);
+            report_error_new(&self.engine_state, &err);
+            return false;
+        }
+
+        let result = eval_block(
+            &self.engine_state,
             &mut self.stack,
-            source.as_bytes(),
-            "build.quake",
+            &block,
             PipelineData::Empty,
-            true,
+            false,
+            false,
         );
-        Ok(())
+
+        match result {
+            Ok(pipeline_data) => {
+                let result;
+                if let PipelineData::ExternalStream {
+                    stdout: stream,
+                    stderr: stderr_stream,
+                    exit_code,
+                    ..
+                } = pipeline_data
+                {
+                    result = print_if_stream(stream, stderr_stream, false, exit_code);
+                } else {
+                    result = pipeline_data.print(&self.engine_state, &mut self.stack, true, false);
+                }
+
+                match result {
+                    Err(err) => {
+                        report_error_new(&self.engine_state, &err);
+                        return false;
+                    }
+                    Ok(exit_code) => {
+                        set_last_exit_code(&mut self.stack, exit_code);
+                    }
+                }
+
+                // reset vt processing, aka ansi because illbehaved externals can break it
+                #[cfg(windows)]
+                {
+                    let _ = enable_vt_processing();
+                }
+            }
+            Err(err) => {
+                set_last_exit_code(&mut self.stack, 1);
+                report_error_new(&self.engine_state, &err);
+                return false;
+            }
+        }
+        true
     }
 }
